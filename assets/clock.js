f68e1cb7be3c4334a1d13319900c1c659938b61f
pongdang <pongdang0121@gmail.com>
2021-07-18 14:53:51 +0900

    구브라우저 지원 함수 변경

--- assets/clock.js
+++ assets/clock.js
@@ -1,5 +1,5 @@
 var clock=document.querySelector("#clock");
 clock.innerHTML='<h1>'+(new Date().toLocaleDateString())+' '+(new Date().toLocaleTimeString())+'</h1>'
-setInterval(() => {
+setInterval(function() {
     clock.innerHTML='<h1>'+(new Date().toLocaleDateString())+' '+(new Date().toLocaleTimeString())+'</h1>'
 }, 1000);
