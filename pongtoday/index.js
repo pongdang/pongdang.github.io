8f56d40dd635e8f43bdfe5effa728a6cd9f1de9b
pongdang <pongdang0121@gmail.com>
2021-09-27 10:58:28 +0900

    html 파일과 script 파일 분리

---
+++ pongtoday/index.js
@@ -0,0 +1,22 @@
+const todayNav = document.querySelector(".todayNav");
+const diaryList = [
+  { name: "2021.08.25, 평균구하기", url: "/pongtoday/08/25" },
+  { name: "2021.08.26, 하샤드 수", url: "/pongtoday/08/26" },
+  { name: "2021.08.27, 핸드폰 번호 가리기", url: "/pongtoday/08/27" },
+  { name: "2021.08.28, x만큼 간격이 있는 n개의 숫자", url: "/pongtoday/08/28" },
+  { name: "2021.08.29, 작은 수 제거하기", url: "/pongtoday/08/29" },
+  { name: "2021.08.31, 정수 제곱근 판별", url: "/pongtoday/08/31" },
+  { name: "2021.09.23, 이상한 문자 만들기", url: "/pongtoday/09/23" },
+  { name: "2021.09.26, 없는 숫자 더하기", url: "/pongtoday/09/26" },
+  { name: "2021.09.27, 음양 더하기", url: "/pongtoday/09/27" },
+];
+for (let i = 0; i < diaryList.length; i++) {
+  const diary = diaryList[i];
+  const liTag = document.createElement("li");
+  const aTag = document.createElement("a");
+  aTag.classList.add("todayItem");
+  aTag.innerText = diary.name;
+  aTag.setAttribute("href", diary.url);
+  liTag.append(aTag);
+  todayNav.append(liTag);
+}
