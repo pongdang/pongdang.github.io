1e1f7d428bf5a154edeff59af525b89d34e714e3
pongdang <pongdang0121@gmail.com>
2021-11-29 16:34:28 +0900

    1129 문제 풀었따

--- pongtoday/index.js
+++ pongtoday/index.js
@@ -47,6 +47,7 @@ const diaryList = [
   { name: "21.11.15, 가장 큰 수", url: "/pongtoday/11/15" },
   { name: "21.11.16, 방문 길이", url: "/pongtoday/11/16" },
   { name: "21.11.22, n^2 배열 자르기", url: "/pongtoday/11/22" },
+  { name: "21.11.22, 이진 변환 반복하기", url: "/pongtoday/11/29" },
 ].reverse();
 
 function markPageNo(pageNo) {
